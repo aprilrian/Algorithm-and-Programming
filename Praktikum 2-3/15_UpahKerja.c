71e73b36250e20837534a2db38cb2879f781bb95
aprilrian <72153636+aprilrian@users.noreply.github.com>
2022-03-17 15:55:03 +0700

    Add files via upload

---
+++ Praktikum 2-3/15_UpahKerja.c
@@ -0,0 +1,61 @@
+/* Nama file	: 15_UpahKerja.c */
+/* Deskripsi	: Menghitung dan menampilkan upah yang diterima */
+/* Nama 		: Aprilyanto Setiyawan Siburian */
+/* NIM  		: 24060121120022 */
+
+#include <stdio.h>
+
+int main(){
+    int Gol, Jam, Upah;
+    printf("MENGHITUNG UPAH YANG DITERIMA\n");
+    printf("-------------------------------");
+    printf("\nMasukkan jenis golongan [1/2/3/4] : ");
+    scanf("%d", &Gol);
+    printf("Masukkan banyaknya jam kerja per minggu : ");
+    scanf("%d", &Jam);
+
+    if ((Gol >= 1 && Gol <= 4) && (Jam > 0)){
+        if (Jam < 40){
+            switch (Gol){
+                case 1 :
+                    printf("Upah yang diterima sebesar Rp %d", Upah = 1000*Jam);
+                    break;
+                case 2 :
+                    printf("Upah yang diterima sebesar Rp %d", Upah = 1500*Jam);
+                    break;
+                case 3 :
+                    printf("Upah yang diterima sebesar Rp %d", Upah = 2000*Jam);
+                    break;
+                case 4 :
+                    printf("Upah yang diterima sebesar Rp %d", Upah = 2500*Jam);
+                    break;
+                default :
+                    printf("Masukan golongan tidak valid");
+            }
+        }
+        else{
+            switch (Gol){
+                case 1 :
+                    printf("Upah yang diterima sebesar Rp %d", Upah = (1000*40)+((Jam-40)*1500));
+                    break;
+                case 2 :
+                    printf("Upah yang diterima sebesar Rp %d", Upah = (1500*40)+((Jam-40)*2250));
+                    break;
+                case 3 :
+                    printf("Upah yang diterima sebesar Rp %d", Upah = (2000*40)+((Jam-40)*3000));
+                    break;
+                case 4 :
+                    printf("Upah yang diterima sebesar Rp %d", Upah = (2500*40)+((Jam-40)*3750));
+                    break;
+                default :
+                    printf("Masukan golongan tidak valid");
+                    break;
+            }
+        }
+    }
+    else{
+        printf("Masukan tidak valid.");
+    }
+
+    return 0;
+}
---
+++ Praktikum 2-3/17_TarifPLN.c
@@ -0,0 +1,59 @@
+/* Nama file	: 17_TarifPLN.c */
+/* Deskripsi	: Menghitung dan menampilkan besarnya tarif listrik
+yang dikenakan */
+/* Nama 		: Aprilyanto Setiyawan Siburian */
+/* NIM  		: 24060121120022 */
+
+#include <stdio.h>
+
+int main(){
+    int Gol, Daya, Tarif;
+    printf("MENGHITUNG BESARNYA TARIF LISTRIK YANG DIKENAKAN\n");
+    printf("------------------------------------------------\n");
+    printf("Masukkan golongan tarif anda [1/2] : ");
+    scanf("%d", &Gol);
+    printf("Masukkan pemakaian daya listrik anda (dalam kWH) : ");
+    scanf("%d", &Daya);
+
+    if ((Gol >= 1) && (Gol <= 2) && (Daya > 0)){
+        if (Daya < 100){
+            if (Gol == 1){
+                printf("\n[PEMAKAIAN DAYA LISTRIK BELUM MEMENUHI MINIMUM PEMBAYARAN]\n\n");
+                printf("Besar tarif listrik yang dikenakan sebesar Rp %d",
+                       Tarif = 1000*Daya);
+            }
+            else{
+                printf("\n[PEMAKAIAN DAYA LISTRIK BELUM MEMENUHI MINIMUM PEMBAYARAN]\n\n");
+                printf("Besar tarif listrik yang dikenakan sebesar Rp %d",
+                       Tarif = 2000*Daya);
+            }
+        }
+        else if ((Daya >= 100) && (Daya < 1000)){
+            if (Gol == 1){
+                printf("Besar tarif listrik yang dikenakan sebesar Rp %d",
+                       Tarif = 1000*Daya);
+            }
+            else {
+                printf("Besar tarif listrik yang dikenakan sebesar Rp %d",
+                       Tarif = 2000*Daya);
+            }
+        }
+        else {
+            if (Gol == 1){
+                Tarif = 1000*Daya;
+                printf("Besar tarif listrik yang dikenakan sebesar Rp %d",
+                       Tarif = Tarif+(Tarif*0.1));
+            }
+            else{
+                Tarif == 2000*Daya;
+                printf("Besar tarif listrik yang dikenakan sebesar Rp %d",
+                       Tarif = Tarif+(Tarif*0.1));
+            }
+        }
+    }
+    else {
+        printf("Masukan tidak valid.");
+    }
+
+    return 0;
+}
